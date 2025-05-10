03754a74c83a5805ac6afd8720d20473b7f23364
albertosaurus_ac <molikodev@gmail.com>
2025-05-10 04:37:23 -0300

    loader moved to hydrate

--- src/webview/HydrateBruno.tsx
+++ src/webview/HydrateBruno.tsx
@@ -3,16 +3,23 @@ import { BruProvider } from "src/webview/context/BruProvider";
 import App from "./App";
 import { TimelineProvider } from "./context/TimeLineProvider";
 import { EditorConfigProvider } from "./context/EditorProvider";
+import { loader } from "@monaco-editor/react";
+
+loader.config({
+    paths: { vs: (globalThis as any).MONACO_BASE_PATH },
+});
 
 const container = document.getElementById("root");
 
-if (container)
-    createRoot(container).render(
+if (container) {
+    (globalThis as any).__BRUNO_ROOT__ ??= createRoot(container);
+    (globalThis as any).__BRUNO_ROOT__.render(
         <EditorConfigProvider>
             <BruProvider>
                 <TimelineProvider>
-                    <App></App>
+                    <App />
                 </TimelineProvider>
             </BruProvider>
         </EditorConfigProvider>
-    )
+    );
+}
