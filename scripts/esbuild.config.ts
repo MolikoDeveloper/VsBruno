545618ab6b93914b807b00cfd63a726f521aee54
albertosaurus_ac <molikodev@gmail.com>
2025-05-10 04:35:09 -0300

    # codemirror removed.

--- scripts/esbuild.config.ts
+++ scripts/esbuild.config.ts
@@ -8,8 +8,7 @@ const webviewEntryPoints = [
     "./src/webview/HydrateBruno.tsx",
     "./src/webview/HydrateCollection.tsx",
     "./src/webview/HydrateEnvironments.tsx",
-    "./src/tailwind.css",
-    "./src/common/highlight.min.cjs"
+    "./src/tailwind.css"
 ];
 
 const prod = process.env.NODE_ENV === 'production';
@@ -39,24 +38,19 @@ export const react: BuildOptions = {
         copyMonacoAssetsPlugin({
             src: "node_modules/monaco-editor/min/vs",
             dest: "dist/vendor/monaco-editor/vs"
-        }),
-    ],
-    alias: {
-        "style-mod": require.resolve("src/vendor/style-mod/src/style-mod.js"),
-        "@lezer/common": require.resolve("src/vendor/@lezer/common/dist/index.js"),
-        "@lezer/highlight": require.resolve("src/vendor/@lezer/highlight/dist/index.js"),
-        "@codemirror/view": require.resolve("src/vendor/@codemirror/view/dist/index.js"),
-    }
+        })
+    ]
 }
 
 export const extension: BuildOptions = {
     entryPoints: ['src/extension.ts'],
     bundle: true,
+    logLevel: "info",
     platform: 'node',
     format: 'cjs',
     target: 'node18',
-    sourcemap: !prod ? 'inline' : false,
-    minify: !prod,
+    sourcemap: false,
+    minify: true,
     outfile: 'dist/extension.cjs',
     external: ['vscode', 'fsevents'],
 }
@@ -64,6 +58,7 @@ export const extension: BuildOptions = {
 export const prelude: BuildOptions = {
     entryPoints: ["src/sandbox/prelude.ts"],
     outdir: "dist/sandbox",
+    logLevel: "info",
     bundle: true,
     platform: "browser",
     format: "iife",
